e8548a042a605d1dae52be0b325486d0eb391c17
aqiongbei <aqiongbei@gmail.com>
2020-06-15 00:28:45 +0800

    modify browser action to page action

--- chrome_extension/background.js
+++ chrome_extension/background.js
@@ -1,27 +1,51 @@
-// chrome.runtime.onInstalled.addListener(function () {
-//     chrome.declarativeContent.onPageChanged.removeRules(undefined, function () {
-//         chrome.declarativeContent.onPageChanged.addRules([
-//             {
-//                 conditions: [
-//                     // 打开QQ空间显示pageAction
-//                     new chrome.declarativeContent.PageStateMatcher({
-//                         pageUrl: {
-//                             hostEquals: 'qq.com',
-//                             schemes: ['https']
-//                         }
-//                     })
-//                 ],
-//                 actions: [new chrome.declarativeContent.ShowPageAction()]
-//             }
-//         ]);
+// chrome.runtime.onInstalled.addListener(function() {
+//     chrome.declarativeContent.onPageChanged.removeRules(undefined, function() {
+//         console.log('page change')
+//         chrome.declarativeContent.onPageChanged.addRules([{
+//             conditions: [
+//                 new chrome.declarativeContent.PageStateMatcher({
+//                     pageUrl: {
+//                         hostContains: 'qq.com'
+//                     }
+//                 })
+//             ],
+//             actions: [new chrome.declarativeContent.ShowPageAction()]
+//         }]);
 //     });
 // });
+chrome.runtime.onInstalled.addListener(function() {
+  // Replace all rules ...
+  chrome.declarativeContent.onPageChanged.removeRules(undefined, function() {
+    // With a new rule ...
+    chrome.declarativeContent.onPageChanged.addRules([
+      {
+        // That fires when a page's URL contains a 'g' ...
+        conditions: [
+          new chrome.declarativeContent.PageStateMatcher({
+            pageUrl: { urlContains: 'qq.com' },
+          })
+        ],
+        // And shows the extension's page action.
+        actions: [ new chrome.declarativeContent.ShowPageAction() ]
+      }
+    ]);
+  });
+});
 
+chrome.tabs.onSelectionChanged.addListener(function(){
+    console.log(chrome)
+    chrome.tabs.query({active: true}, function (tab) {
+        console.log('tab', tab);
+    });
+    // if (tab.url.toLowerCase().indexOf("zhihu.com") > -1){
+    //     chrome.pageAction.show(id);
+    // }
+});
 console.log('load background.js');
 // 获取当前选项卡ID
 function getCurrentTabId(callback) {
-    chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
-        if(callback) callback(tabs.length ? tabs[0].id: null);
+    chrome.tabs.query({ active: true, currentWindow: true }, function(tabs) {
+        if (callback) callback(tabs.length ? tabs[0].id : null);
     });
 }
 
@@ -35,9 +59,9 @@ function msg2Content(message, callback) {
 
 chrome.runtime.onMessage.addListener(function(request, sender, sendResponse) {
     console.log('background.js recive:', request)
-    sendResponse({is_recive: true, orion: 'background.js'});
+    sendResponse({ is_recive: true, orion: 'background.js' });
 })
 
-function notice (opt) {
+function notice(opt) {
     msg2Content(opt);
 }
