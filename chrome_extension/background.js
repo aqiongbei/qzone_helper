d6a7f2cd440fbf462e6a9251bdbd4ac9ad855457
aqiongbei <aqiongbei@gmail.com>
2020-06-15 16:40:30 +0800

    fix #8

--- chrome_extension/background.js
+++ chrome_extension/background.js
@@ -1,45 +1,16 @@
-// chrome.runtime.onInstalled.addListener(function() {
-//     chrome.declarativeContent.onPageChanged.removeRules(undefined, function() {
-//         console.log('page change')
-//         chrome.declarativeContent.onPageChanged.addRules([{
-//             conditions: [
-//                 new chrome.declarativeContent.PageStateMatcher({
-//                     pageUrl: {
-//                         hostContains: 'qq.com'
-//                     }
-//                 })
-//             ],
-//             actions: [new chrome.declarativeContent.ShowPageAction()]
-//         }]);
-//     });
-// });
 chrome.runtime.onInstalled.addListener(function() {
-  // Replace all rules ...
-  chrome.declarativeContent.onPageChanged.removeRules(undefined, function() {
-    // With a new rule ...
-    chrome.declarativeContent.onPageChanged.addRules([
-      {
-        // That fires when a page's URL contains a 'g' ...
-        conditions: [
-          new chrome.declarativeContent.PageStateMatcher({
-            pageUrl: { urlContains: 'qq.com' },
-          })
-        ],
-        // And shows the extension's page action.
-        actions: [ new chrome.declarativeContent.ShowPageAction() ]
-      }
-    ]);
-  });
-});
-
-chrome.tabs.onSelectionChanged.addListener(function(){
-    console.log(chrome)
-    chrome.tabs.query({active: true}, function (tab) {
-        console.log('tab', tab);
+    chrome.declarativeContent.onPageChanged.removeRules(undefined, function() {
+        chrome.declarativeContent.onPageChanged.addRules([{
+            conditions: [
+                new chrome.declarativeContent.PageStateMatcher({
+                    pageUrl: {
+                        hostContains: 'qzone.qq.com'
+                    }
+                })
+            ],
+            actions: [new chrome.declarativeContent.ShowPageAction()]
+        }]);
     });
-    // if (tab.url.toLowerCase().indexOf("zhihu.com") > -1){
-    //     chrome.pageAction.show(id);
-    // }
 });
 console.log('load background.js');
 // 获取当前选项卡ID
