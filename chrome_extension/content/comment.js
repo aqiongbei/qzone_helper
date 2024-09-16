9fcdfaa42f94772fef52e779fb582f3ae31cf926
aqiongbei <aqiongbei@gmail.com>
2024-09-16 21:34:17 +0800

    rebuild chrome extension

--- chrome_extension/content/comment.js
+++ chrome_extension/content/comment.js
@@ -1,117 +1,127 @@
-console.log('comment.js loaded')
+console.log("comment.js loaded");
 if (!window.TASK) {
-    window.TASK = {};
+  window.TASK = {};
 }
 window.TASK.comment = {
-    getComment (opt, cb) {
-        let config = {
-            url: 'https://user.qzone.qq.com/proxy/domain/m.qzone.qq.com/cgi-bin/new/get_msgb',
-            method: 'GET',
-            qs: {
-                uin: base_info.uin,
-                hostUin: base_info.uin,
-                sort: 0,
-                s: 0.7434565759252794,
-                format: 'json',
-                num: 10,
-                start: 0,
-                inCharset: 'utf-8',
-                outCharset: 'utf-8',
-                g_tk: base_info.g_tk,
-                qzonetoken: base_info.token,
-                g_tk: base_info.g_tk
-            }
-        };
-        if (opt && opt.qs) {
-            config.qs = Object.assign(config.qs, opt.qs);
-        }
-        if (opt && opt.data) {
-            config.data = Object.assign(config.data, opt.data);
-        }
-        utils.request(config, function (res) {
-            utils.showTips(`获取留言列表成功! 当前进度: ${window.comment_list.length} / ${res.data.total}`);
-            cb && cb(res);
-        });
-    },
-    deleteComment () {
-        let uin_list = [];
-        let id_list = [];
-        window.comment_list.map(comment => {
-            uin_list.push(base_info.uin);
-            id_list.push(comment.id);
-        })
-        utils.request({
-            url: 'https://h5.qzone.qq.com/proxy/domain/m.qzone.qq.com/cgi-bin/new/del_msgb',
-            method: 'POST',
-            qs: {
-                qzonetoken: base_info.token,
-                g_tk: base_info.g_tk,
-            },
-            data: {
-                hostUin: base_info.uin,
-                idList: id_list.join(),
-                uinList: uin_list.join(),
-                format: 'fs',
-                iNotice: 1,
-                inCharset: 'utf-8',
-                outCharset: 'utf-8',
-                ref: 'qzone',
-                json: 1,
-                g_tk: base_info.g_tk,
-                qzreferrer: 'https://user.qzone.qq.com/proxy/domain/qzs.qq.com/qzone/msgboard/msgbcanvas.html#page=1'
-            }
-        }, function (res) {
-            if (res.includes('成功删除')) {
-                utils.showTips(`删除留言${uin_list.length}成功!`);
-                window.comment_list = [];
-            } else {
-                utils.showTips(`删除留言${uin_list.length}失败:\n${res}!`);
-            }
-        });
-        utils.next( () => {
-            setTimeout( () => {
-                if (window.comment_list && window.comment_list.length) {
-                    this.deleteComment();
-                } else {
-                    this.delete();
-                }
-            }, 6000);
-        })
-    },
-    delete () {
-        let foo = res => {
-            if (res.data && res.data.total) {
-                window.comment_list = res.data.commentList;
-                utils.next( () => {
-                    this.deleteComment();
-                });
-            } else {
-                utils.showTips('删除留言完成!', 'succeed');
-            }
-        }
-        this.getComment(null, foo);
-    },
-    export () {
-        let foo = res => {
-            console.log('cb', res)
-            if (res.data && res.data.commentList) {
-                window.comment_list = window.comment_list.concat(res.data.commentList)
-            }
-            if (window.comment_list.length >= res.data.total) {
-                utils.download(JSON.stringify(window.comment_list, 4, ' '), `comment_list.json`);
-                window.comment_list = [];
-                utils.showTips('导出留言完成!', 'succeed');
-            } else {
-                let opt = {
-                    qs: {
-                        start: window.comment_list.length
-                    }
-                }
-                utils.next(() => {
-                    this.getComment(opt, foo);
-                })
-            }
-        }
-        this.getComment(null, foo);
+  getComment(opt, cb) {
+    let config = {
+      url: "https://user.qzone.qq.com/proxy/domain/m.qzone.qq.com/cgi-bin/new/get_msgb",
+      method: "GET",
+      qs: {
+        uin: base_info.uin,
+        hostUin: base_info.uin,
+        sort: 0,
+        s: 0.7434565759252794,
+        format: "json",
+        num: 10,
+        start: 0,
+        inCharset: "utf-8",
+        outCharset: "utf-8",
+        g_tk: base_info.g_tk,
+        qzonetoken: base_info.token,
+        g_tk: base_info.g_tk,
+      },
+    };
+    if (opt && opt.qs) {
+      config.qs = Object.assign(config.qs, opt.qs);
     }
-}
+    if (opt && opt.data) {
+      config.data = Object.assign(config.data, opt.data);
+    }
+    utils.request(config, function (res) {
+      utils.showTips(
+        `获取留言列表成功! 当前进度: ${window.comment_list.length} / ${res.data.total}`
+      );
+      cb && cb(res);
+    });
+  },
+  deleteComment() {
+    let uin_list = [];
+    let id_list = [];
+    window.comment_list.map((comment) => {
+      uin_list.push(base_info.uin);
+      id_list.push(comment.id);
+    });
+    utils.request(
+      {
+        url: "https://h5.qzone.qq.com/proxy/domain/m.qzone.qq.com/cgi-bin/new/del_msgb",
+        method: "POST",
+        qs: {
+          qzonetoken: base_info.token,
+          g_tk: base_info.g_tk,
+        },
+        data: {
+          hostUin: base_info.uin,
+          idList: id_list.join(),
+          uinList: uin_list.join(),
+          format: "fs",
+          iNotice: 1,
+          inCharset: "utf-8",
+          outCharset: "utf-8",
+          ref: "qzone",
+          json: 1,
+          g_tk: base_info.g_tk,
+          qzreferrer:
+            "https://user.qzone.qq.com/proxy/domain/qzs.qq.com/qzone/msgboard/msgbcanvas.html#page=1",
+        },
+      },
+      function (res) {
+        if (res.includes("成功删除")) {
+          utils.showTips(`删除留言${uin_list.length}成功!`);
+          window.comment_list = [];
+        } else {
+          utils.showTips(`删除留言${uin_list.length}失败:\n${res}!`);
+        }
+      }
+    );
+    utils.next(() => {
+      setTimeout(() => {
+        if (window.comment_list && window.comment_list.length) {
+          this.deleteComment();
+        } else {
+          this.delete();
+        }
+      }, 6000);
+    });
+  },
+  delete() {
+    let foo = (res) => {
+      if (res.data && res.data.total) {
+        window.comment_list = res.data.commentList;
+        utils.next(() => {
+          this.deleteComment();
+        });
+      } else {
+        utils.showTips("删除留言完成!", "succeed");
+        window.location.reload();
+      }
+    };
+    this.getComment(null, foo);
+  },
+  export() {
+    let foo = (res) => {
+      console.log("cb", res);
+      if (res.data && res.data.commentList) {
+        window.comment_list = window.comment_list.concat(res.data.commentList);
+      }
+      if (window.comment_list.length >= res.data.total) {
+        utils.download(
+          JSON.stringify(window.comment_list, 4, " "),
+          `comment_list.json`
+        );
+        window.comment_list = [];
+        utils.showTips("导出留言完成!", "succeed");
+      } else {
+        let opt = {
+          qs: {
+            start: window.comment_list.length,
+          },
+        };
+        utils.next(() => {
+          this.getComment(opt, foo);
+        });
+      }
+    };
+    this.getComment(null, foo);
+  },
+};
--- chrome_extension/content/index.js
+++ chrome_extension/content/index.js
@@ -1,102 +1,144 @@
-console.log('content js loaded');
+console.log("content_scripts loaded");
 
-function preCheck () {
-    // https://rc.qzone.qq.com/infocenter?via=toolbar&_t_=0.3928029360436276&
-    // to
-    // https://user.qzone.qq.com/xxx
-    if (!window.location.href.includes('user.qzone.qq.com')) {
-        window.location.href = 'https://qzone.qq.com';
+function injectUI() {
+  let { name, author, version, homepage_url } = chrome.runtime.getManifest();
+  let wrapperEl = document.createElement("div");
+  wrapperEl.setAttribute("id", "qzone_helper-wrapper");
+  wrapperEl.innerHTML = `
+    <div class="top">
+      <h3>${name}</h3>
+      <span id="qzone_helper-closeBtn">关闭</span>
+    </div>
+    <hr/>
+    <button data-action="export_talk" title="将说说的请求数据导出为json格式的文本,包含评论">
+        导出说说
+      </button>
+      <button data-action="delete_talk" class="warn">删除说说</button>
+      <br />
+      <br />
+      <button data-action="export_comment" title="将留言的请求数据导出为json格式的文本,包含评论">
+        导出留言
+      </button>
+      <button data-action="delete_comment" class="warn">删除留言</button>
+      <br />
+      <br />
+      <button data-action="stop_all" class="long">
+        终止任务
+      </button>
+      <hr/>
+      <footer>
+      version: ${version} author: ${author} <a href=${homepage_url}>Github</a>
+    </footer>
+    </main>
+    `;
+  document.body.appendChild(wrapperEl);
+
+  wrapperEl.addEventListener("click", function (e) {
+    console.log("e", e);
+    e.target.disabled = true;
+    let action = e.target.getAttribute("data-action");
+    if (action) {
+      dispatch(action);
     }
+  });
+
+  document
+    .getElementById("qzone_helper-closeBtn")
+    .addEventListener("click", function () {
+      document.body.removeChild(wrapperEl);
+    });
 }
 
-function init () {
-    window.comment_list = [];
-    window.talk_list = [];
-    window.blog_list = [];
-    window.comment_total = 0;
-    window.comment_has_total = 0;
-    window.talk_total = 0;
-    window.blog_total = 0;
-    window.comment_page = 0;
-    window.base_info = utils.getBaseInfo();
-    window.stop_task = false;
-    window.tip_queue = [];
+function preCheck() {
+  // https://rc.qzone.qq.com/infocenter?via=toolbar&_t_=0.3928029360436276&
+  // to
+  // https://user.qzone.qq.com/xxx
+  if (!window.location.href.includes("user.qzone.qq.com")) {
+    window.location.href = "https://qzone.qq.com";
+  }
 }
 
-function notice (msg) {
-    chrome.runtime.sendMessage('msg')
+function init() {
+  window.comment_list = [];
+  window.talk_list = [];
+  window.blog_list = [];
+  window.comment_total = 0;
+  window.comment_has_total = 0;
+  window.talk_total = 0;
+  window.blog_total = 0;
+  window.comment_page = 0;
+  window.base_info = utils.getBaseInfo();
+  window.stop_task = false;
+  window.tip_queue = [];
 }
+
 const TASK_TYPE = {
-    export_talk: {
-        key: 'export_talk',
-        name: '导出说说'
-    },
-    delete_talk: {
-        key: 'delete_talk',
-        name: '删除说说'
-    },
-    export_comment: {
-        key: 'export_comment',
-        name: '导出留言'
-    },
-    delete_comment: {
-        key: 'delete_comment',
-        name: '删除留言'
-    },
-    export_blog: {
-        key: 'export_blog',
-        name: '导出日志'
-    },
-    delete_blog: {
-        key: 'delete_blog',
-        name: '删除日志'
-    },
-    export_photo: {
-        key: 'export_photo',
-        name: '导出相册'
-    },
-    delete_photo: {
-        key: 'delete_photo',
-        name: '删除相册'
-    },
-    delete_all: {
-        key: 'delete_all',
-        name: '一键清空说说+留言+日志+相册'
-    }
+  export_talk: {
+    key: "export_talk",
+    name: "导出说说",
+  },
+  delete_talk: {
+    key: "delete_talk",
+    name: "删除说说",
+  },
+  export_comment: {
+    key: "export_comment",
+    name: "导出留言",
+  },
+  delete_comment: {
+    key: "delete_comment",
+    name: "删除留言",
+  },
+  export_blog: {
+    key: "export_blog",
+    name: "导出日志",
+  },
+  delete_blog: {
+    key: "delete_blog",
+    name: "删除日志",
+  },
+  export_photo: {
+    key: "export_photo",
+    name: "导出相册",
+  },
+  delete_photo: {
+    key: "delete_photo",
+    name: "删除相册",
+  },
+  delete_all: {
+    key: "delete_all",
+    name: "一键清空说说+留言+日志+相册",
+  },
 };
-function dispatch (opt) {
-    preCheck();
-    init();
-    if (opt.type == 'stop_all') {
-        window.stop_task = true;
-        utils.showTips(`终止任务成功!`, 'succeed')
-        return;
-    }
-    utils.showTips(`开始处理 < ${TASK_TYPE[opt.type].name} > 任务`)
-    switch (opt.type) {
-        case TASK_TYPE.export_talk.key:
-            TASK.talk.export();
-            break;
-        case TASK_TYPE.delete_talk.key:
-            TASK.talk.delete();
-            break;
-        case TASK_TYPE.export_comment.key:
-            TASK.comment.export();
-            break;
-        case TASK_TYPE.delete_comment.key:
-            TASK.comment.delete();
-            break;
-        default :
-            utils.showTips(`暂不支持 < ${TASK_TYPE[opt.type].name} > 任务`);
-            break;
-    }
+
+function dispatch(action) {
+  preCheck();
+  init();
+
+  if (action == "stop_all") {
+    window.stop_task = true;
+    utils.showTips(`终止任务成功!`, "succeed");
+    return;
+  }
+
+  utils.showTips(`开始处理 < ${TASK_TYPE[action].name} > 任务`);
+  switch (action) {
+    case TASK_TYPE.export_talk.key:
+      TASK.talk.export();
+      break;
+    case TASK_TYPE.delete_talk.key:
+      TASK.talk.delete();
+      break;
+    case TASK_TYPE.export_comment.key:
+      TASK.comment.export();
+      break;
+    case TASK_TYPE.delete_comment.key:
+      TASK.comment.delete();
+      break;
+    default:
+      utils.showTips(`暂不支持 < ${TASK_TYPE[action].name} > 任务`);
+      break;
+  }
 }
-chrome.runtime.onMessage.addListener((message, sender, sendResponse) => {
-    console.log('content.js recive msg:', message, sender);
-    sendResponse({is_recive: true, orion: 'content.js'});
-    try {
-        dispatch(message);
-    } catch (e) {
-        console.log(e);
-    }
-})
+
+injectUI();
